dad55921d5e88cef1f575f0997cd84cdeee6f6c1
delphism84 <97236522+delphism84@users.noreply.github.com>
2026-01-22 19:05:45 +0900

    chore(docker): clean up unused docker-compose files and streamline configuration

--- docker/mongo-init/init.js
+++
@@ -1,7 +0,0 @@
-// create initial db
-var dbName = 'lunarUniScan';
-var dbRef = db.getSiblingDB(dbName);
-// touch a collection to ensure creation
-if (!dbRef.getCollectionNames().includes('init')) {
-  dbRef.createCollection('init');
-}
